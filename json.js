0abc4057aa6efc0cefc33670a30c63e05bf80f6f
ArjunVenkitraman <ramukpully@gmail.com>
2023-05-03 09:12:54 +0530

    lapcommit

--- json.js
+++ json.js
@@ -16,8 +16,10 @@ function loadTable() {
           trHTML += "<td>" + object["AreaSqFt"] + "</td>";
           trHTML += "<td>" + object["ConstructedYear"] + "</td>";
           trHTML += "<td>" + object["Rate"] + "</td>";
-          trHTML += "<td>" + object["Image"] + "</td>";
-
+          trHTML +=
+            '<td><img width="50px" src="' +
+            object["Image"] +
+            '" class="avatar"></td>';
           trHTML +=
             '<td><button type="button" class="btn btn-outline-secondary" onclick="showUserEditBox(' +
             object["id"] +
@@ -35,17 +37,60 @@ function loadTable() {
   
   loadTable();
 
+  function userSearch(BuildingType) {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4) {
+        if (this.status == 200) {
+          console.log(this.responseText);
+          var trHTML = "";
+          const objects = JSON.parse(this.responseText);
+          for (let object of objects) {
+            trHTML += "<tr>";
+            trHTML += "<td>" + object["id"] + "</td>";
+            trHTML += "<td>" + object["BuildingType"] + "</td>";
+            trHTML += "<td>" + object["Address"] + "</td>";
+            trHTML += "<td>" + object["AreaSqFt"] + "</td>";
+            trHTML += "<td>" + object["ConstructedYear"] + "</td>";
+            trHTML += "<td>" + object["Rate"] + "</td>";
+            trHTML +=
+              '<td><img width="50px" src="' +
+              object["Image"] +
+              '" class="avatar"></td>';
+            trHTML +=
+              '<td><button type="button" class="btn btn-outline-secondary" onclick="showUserEditBox(' +
+              object["id"] +
+              ')">Edit</button>';
+            trHTML +=
+              '<button type="button" class="btn btn-outline-danger" onclick="userDelete(' +
+              object["id"] +
+              ')">Del</button></td>';
+            trHTML += "</tr>";
+          }
+          document.getElementById("mytable").innerHTML = trHTML;
+        } else {
+          console.error("Error fetching data:", this.status, this.statusText);
+        }
+      }
+    };
+    xhttp.open(
+      "GET",
+      `http://localhost:3000/buildingSale?BuildingType=${BuildingType}`
+    );
+    xhttp.send();
+  }
+
   function showUserCreateBox() {
     Swal.fire({
       title: "Create user",
       html:
         '<input id="id" type="hidden">' +
-        '<input id="BuildingType" class="swal2-input" placeholder="First">' +
-        '<input id="Address" class="swal2-input" placeholder="Last">' +
-        '<input id="AreaSqFt" class="swal2-input" placeholder="Username">' +
-        '<input id="ConstructedYear" class="swal2-input" placeholder="Email">'+
-        '<input id="Rate" class="swal2-input" placeholder="Email">'+
-        '<input id="Image" class="swal2-input" placeholder="Email">',
+        '<input id="BuildingType" class="swal2-input" placeholder="BuildingType">' +
+        '<input id="Address" class="swal2-input" placeholder="Address">' +
+        '<input id="AreaSqFt" class="swal2-input" placeholder="AreaSqFt">' +
+        '<input id="ConstructedYear" class="swal2-input" type="date" placeholder="ConstructedYear">'+
+        '<input id="Rate" class="swal2-input" placeholder="Rate">'+
+        '<input id="Image" class="swal2-input" placeholder="Image">',
       preConfirm: () => {
         userCreate();
       },
@@ -104,10 +149,10 @@ function loadTable() {
             '<input id="id" type="hidden" value="' +
             objects[`${id}`] +
             '">' +
-            '<input id="BuildingType" class="swal2-input" placeholder="First" value="' +
+            '<input id="BuildingType" class="swal2-input" placeholder="BuildingType" value="' +
             objects["BuildingType"] +
             '">' +
-            '<input id="Address" class="swal2-input" placeholder="Last" value="' +
+            '<input id="Address" class="swal2-input" placeholder="Address" value="' +
             objects["Address"] +
             '">' +
             '<input id="AreaSqFt" class="swal2-input" placeholder="AreaSqFt" value="' +
@@ -138,7 +183,6 @@ function loadTable() {
     
     console.log(id);
     console.log(BuildingType);
-    
     const xhttp = new XMLHttpRequest();
     xhttp.open("PUT", `http://localhost:3000/buildingSale/${id}`);
     xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
@@ -152,23 +196,17 @@ function loadTable() {
         Image: Image,
       })
     );
-    
+  
     xhttp.onreadystatechange = function () {
       if (this.readyState == 4 && this.status == 200) {
         const objects = JSON.parse(this.responseText);
         Swal.fire({
-          title: 'Success',
-          text: objects["message"],
+          title: 'Updated!',
+          text: 'The user has been updated.',
           icon: 'success',
-          timer: 3000, // Display the message for 3 seconds
-          timerProgressBar: true, // Show the progress bar
-          didOpen: () => {
-            Swal.showLoading(); // Show a loading animation until the timer expires
-          },
-          willClose: () => {
-            loadTable(); // Reload the table after editing the user
-          }
+          timerProgressBar: true,
         });
+        loadTable();
       }
     };
   }
@@ -176,13 +214,13 @@ function loadTable() {
   function userDelete(id) {
     console.log(id);
     Swal.fire({
-      title: 'Are you sure?',
+      title: "Are you sure?",
       text: "You won't be able to revert this!",
-      icon: 'warning',
+      icon: "warning",
       showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Yes, delete it!'
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
     }).then((result) => {
       if (result.value) {
         const xhttp = new XMLHttpRequest();
@@ -197,17 +235,11 @@ function loadTable() {
           if (this.readyState == 4) {
             const objects = JSON.parse(this.responseText);
             Swal.fire({
-              title: 'Deleted!',
-              text: 'The user has been deleted.',
-              icon: 'success',
-              timer: 5000, // Display the message for 3 seconds
-              timerProgressBar: true, // Show the progress bar
-              didOpen: () => {
-                Swal.showLoading(); // Show a loading animation until the timer expires
-              },
-              willClose: () => {
-                loadTable(); // Reload the table after deleting the user
-              }
+              title: "Deleted!",
+              text: "The user has been deleted.",
+              icon: "success",
+              timer: 5000, 
+              timerProgressBar: true,
             });
           }
         };
