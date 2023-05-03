14d43d60f15b4f64326d111ab5661e9937522d3d
ArjunVenkitraman <ramukpully@gmail.com>
2023-05-03 23:51:06 +0530

    lap

--- json.js
+++ json.js
@@ -44,37 +44,36 @@ function loadTable() {
         if (this.status == 200) {
           console.log(this.responseText);
           var trHTML = "";
-          const objects = JSON.parse(this.responseText);
-          for (let object of objects) {
-            trHTML += "<tr>";
-            trHTML += "<td>" + object["id"] + "</td>";
-            trHTML += "<td>" + object["BuildingType"] + "</td>";
-            trHTML += "<td>" + object["Address"] + "</td>";
-            trHTML += "<td>" + object["AreaSqFt"] + "</td>";
-            trHTML += "<td>" + object["ConstructedYear"] + "</td>";
-            trHTML += "<td>" + object["Rate"] + "</td>";
-            trHTML +=
-              '<td><img width="50px" src="' +
-              object["Image"] +
-              '" class="avatar"></td>';
-            trHTML +=
-              '<td><button type="button" class="btn btn-outline-secondary" onclick="showUserEditBox(' +
-              object["id"] +
-              ')">Edit</button>';
-            trHTML += 
-              '<button type="button" class="btn btn-outline-danger" onclick="userDelete(' +
-              object["id"] +
-              ')">Del</button></td>';
-            trHTML += "</tr>";
-          }
-          document.getElementById("mytable").innerHTML = trHTML;
-        } else {
-          console.error("Error fetching data:", this.status, this.statusText);
+        const objects = JSON.parse(this.responseText);
+        for (let object of objects) {
+          trHTML += "<tr>";
+          trHTML += "<td>" + object["id"] + "</td>";
+          trHTML += "<td>" + object["BuildingType"] + "</td>";
+          trHTML += "<td>" + object["Address"] + "</td>";
+          trHTML += "<td>" + object["AreaSqFt"] + "</td>";
+          trHTML += "<td>" + object["ConstructedYear"] + "</td>";
+          trHTML += "<td>" + object["Rate"] + "</td>";
+          trHTML +=
+            '<td><img width="50px" src="' +
+            object["Image"] +
+            '" class="avatar"></td>';
+          trHTML +=
+            '<td><button type="button" class="btn btn-outline-secondary" onclick="showUserEditBox(' +
+            object["id"] +
+            ')"><i class="fa-solid fa-file-pen text-warning" "></i></button>&nbsp';
+          trHTML +=
+            '<button type="button" class="btn btn-outline-danger" onclick="userDelete(' +
+            object["id"] +
+            ')"><i class="fa-solid fa-trash  fa-sm" style="color: #ff3300;"></i></button></td>';
+          trHTML += "</tr>";
+        }
+        document.getElementById("mytable").innerHTML = trHTML;
         }
       }
     };
     xhttp.open(
-      "GET",`http://localhost:3000/buildingSale?BuildingType=${BuildingType}`
+      "GET",
+      `http://localhost:3000/buildingSale?BuildingType=${BuildingType}`
     );
     xhttp.send();
   }
@@ -84,12 +83,12 @@ function loadTable() {
       title: "Load building",
       html:
         '<input id="id" type="hidden"><br>' +
-        '<i class="fa-solid fa-building-wheat fa-xl"></i><input id="BuildingType" class="swal2-input" placeholder="BuildingType"><br>' +
+        '<i class="fa-solid fa-building-wheat fa-xl"></i> <select id="BuildingType" class="swal2-input" style="width:62%"> <option value="Residential Building">None</option><option value="Residential Building">Residential Building</option> <option value="Educational Buildings">Educational Buildings</option> <option value="Institutional Buildings">Institutional Buildings</option> <option value="Assembly Buildings">Assembly Buildings</option></select><br>' +
         '<i class="fa-solid fa-address-card fa-xl"></i><input id="Address" class="swal2-input" placeholder="Address"><br>' +
         '<i class="fa-solid fa-chart-area fa-xl"></i></i><input id="AreaSqFt" class="swal2-input" placeholder="AreaSqFt"><br>' +
         '<i class="fa-solid fa-calendar-days fa-xl"></i><input id="ConstructedYear" class="swal2-input" type="date" placeholder="ConstructedYear" style="width:62%"><br>'+
-        '<i class="fa-solid fa-sack-dollar fa-xl"></i><input id="Rate" class="swal2-input" placeholder="Rate"><br>'+
-        '<i class="fa-solid fa-image fa-xl"></i><input type="file" style="width:282px" id="Image" class="swal2-input"  placeholder="Image">',
+        '<i class="fa-solid fa-sack-dollar fa-xl"></i><input id="Rate" class="swal2-input" placeholder="Rate"><br>',
+        // '<i class="fa-solid fa-image fa-xl"></i><input type="file" style="width:282px" id="Image" class="swal2-input"  placeholder="Image">',
       preConfirm: () => {
         userCreate();
       },
@@ -102,9 +101,25 @@ function loadTable() {
     const AreaSqFt = document.getElementById("AreaSqFt").value;
     const ConstructedYear = document.getElementById("ConstructedYear").value;
     const Rate = document.getElementById("Rate").value;
-    const Image = document.getElementById("Image").value;
+    var Image;
   
-   
+    // Set default image if no file is selected
+    
+      if (BuildingType === "Residential Building") {
+        Image = "assets/images/Residential.webp";
+      } else if (BuildingType === "	Educational Buildings") {
+        Image = "assets/images/education.avif";
+      } 
+      else if(BuildingType === "Institutional Buildings"){
+        Image = "assets/images/industrial.jpg";
+      }
+      else if(BuildingType === "Assembly Buildings"){
+        Image = "assets/images/assembly.jpg";
+      }
+      else {
+        Image = "assets/images/default.svg";
+      }
+
     if(validate()==true){
     const xhttp = new XMLHttpRequest();
     xhttp.open("POST", "http://localhost:3000/buildingSale/");
@@ -125,7 +140,7 @@ function loadTable() {
         const objects = JSON.parse(this.responseText);
         Swal.fire({ 
         title:"Hello",
-        icon:"hello",
+        icon:"success",
         showConfirmButton:true,
         timer:1500,
       });
@@ -144,9 +159,8 @@ function loadTable() {
     const Rate= document.getElementById("Rate").value;
 
  //regular expressions
- const areaReg = /^[a-zA-Z\s]{2,32}$/;
- const yearReg = /^[a-zA-Z\s]{2,32}$/;
- const rateReg =/^[a-zA-Z\s]{2,32}$/;
+ const areaReg = /^[0-9]*\.?[0-9]*$/;
+ const rateReg = /^[a-zA-Z0-9.]+$/;
 
  if (BuildingType == "" || Address == "" || AreaSqFt == "" || ConstructedYear == "" || Rate == "") {
      Swal.fire({
@@ -160,17 +174,8 @@ function loadTable() {
      });
      return false;
  }
- if (!BuildingType.match(areaReg)) {
 
-     Swal.fire({
-         title: "Invalid Input",
-         text: "Name should only contain alphabetical letters",
-         icon: "error",
-         showConfirmButton: true,
-     })
-     return false;
- }
- if (!Address.match(yearReg)) {
+ if (!AreaSqFt.match(areaReg)) {
 
      Swal.fire({
          title: "Invalid Input",
@@ -190,7 +195,7 @@ function loadTable() {
      return false;
  }
 
- if (BuildingType.match(areaReg) && Address.match(yearReg) && Rate.match(rateReg)) {
+ if (AreaSqFt.match(areaReg) && Rate.match(rateReg)) {
      Swal.fire({
          title: "Successfully Created",
          icon: "success",
@@ -198,7 +203,21 @@ function loadTable() {
      })
      return true;
  }
+ const xhttp = new XMLHttpRequest();
+ xhttp.open("POST", "http://localhost:3000/buildingSale/");
+ xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+ xhttp.send(
+   JSON.stringify({
+     BuildingType: BuildingType,
+     Address: Address,
+     AreaSqFt: AreaSqFt,
+     ConstructedYear: ConstructedYear,
+     Rate: Rate,
+     Image: Image,
+   })
+ );
 }
+
   function showUserEditBox(id) {
     console.log(id);
     const xhttp = new XMLHttpRequest();
@@ -239,16 +258,16 @@ function loadTable() {
       }
     };
   }
-  function userEdit(id) {
-    const BuildingType = document.getElementById("BuildingType").value;
-    const Address = document.getElementById("Address").value;
-    const AreaSqFt = document.getElementById("AreaSqFt").value;
-    const ConstructedYear = document.getElementById("ConstructedYear").value;
-    const Rate = document.getElementById("Rate").value;
-    const Image = document.getElementById("Image").value;
-    
-    console.log(id);
-    console.log(BuildingType);
+
+ function userEdit(id) {
+  const BuildingType = document.getElementById("BuildingType").value;
+  const Address = document.getElementById("Address").value;
+  const AreaSqFt = document.getElementById("AreaSqFt").value;
+  const ConstructedYear = document.getElementById("ConstructedYear").value;
+  const Rate = document.getElementById("Rate").value;
+  const Image = document.getElementById("Image").value;
+  
+  if (validate()) {
     const xhttp = new XMLHttpRequest();
     xhttp.open("PUT", `http://localhost:3000/buildingSale/${id}`);
     xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
@@ -262,20 +281,69 @@ function loadTable() {
         Image: Image,
       })
     );
-  
+
     xhttp.onreadystatechange = function () {
       if (this.readyState == 4 && this.status == 200) {
         const objects = JSON.parse(this.responseText);
         Swal.fire({
-          title: 'Updated!',
-          text: 'The user has been updated.',
-          icon: 'success',
+          title: "Updated!",
+          text: "The user has been updated.",
+          icon: "success",
           timerProgressBar: true,
         });
         loadTable();
       }
     };
   }
+}
+
+function validate() {
+  const BuildingType = document.getElementById("BuildingType").value;
+  const Address = document.getElementById("Address").value;
+  const AreaSqFt = document.getElementById("AreaSqFt").value;
+  const ConstructedYear = document.getElementById("ConstructedYear").value;
+  const Rate = document.getElementById("Rate").value;
+
+  // Regular expressions
+  const areaReg = /^[0-9]*\.?[0-9]*$/;
+  const rateReg = /^[a-zA-Z0-9.]+$/;
+
+  if (BuildingType == "" || Address == "" || AreaSqFt == "" || ConstructedYear == "" || Rate == "") {
+    Swal.fire({
+      title: "Fields should not be empty",
+      showConfirmButton: true,
+      icon: "error"
+    }).then((res) => {
+      if (res.value) {
+        showUserEditBox();
+      }
+    });
+    return false;
+  }
+
+  if (!AreaSqFt.match(areaReg)) {
+    Swal.fire({
+      title: "Invalid Input",
+      text: "AreaSqFt should only contain numbers",
+      icon: "error",
+      showConfirmButton: true
+    });
+    return false;
+  }
+
+  if (!Rate.match(rateReg)) {
+    Swal.fire({
+      title: "Invalid Input",
+      text: "Please enter a valid Rate",
+      icon: "error",
+      showConfirmButton: true
+    });
+    return false;
+  }
+
+  return true;
+}
+
 
   function userDelete(id) {
     console.log(id);
